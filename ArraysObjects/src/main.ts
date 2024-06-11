b4c8869dbb566167115954ef343c038ff4040714
Clifford Musyoka <your_email@example.com>
2024-06-11 06:22:38 +0300

    Define object data types in advance

--- ArraysObjects/src/main.ts
+++ ArraysObjects/src/main.ts
@@ -32,3 +32,16 @@ const exampleObj = {
 };
 
 exampleObj.prop2 = false;
+
+
+type Guitarist = {
+    name: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+let evh : Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'ou812']
+}
