17b20b1f00f560929498390c69581e12a5242e43
Clifford Musyoka <your_email@example.com>
2024-06-11 06:19:24 +0300

    Define object types

--- ArraysObjects/src/main.ts
+++ ArraysObjects/src/main.ts
@@ -1,11 +1,34 @@
-let stringArr = ['one', 'hey', 'Dave']
+let stringArr = ["one", "hey", "Dave"];
 
-let guitars = ['Strat', 'Les Paul', 5150]
+let guitars = ["Strat", "Les Paul", 5150];
 
-let mixedData = ['EVH', 1984, true]
+let mixedData = ["EVH", 1984, true];
 
-stringArr[0] = 'John'
-stringArr.push('hey')
+stringArr[0] = "John";
+stringArr.push("hey");
 
-guitars[0] = 1984
-guitars.unshift('jim')
+guitars[0] = 1984;
+guitars.unshift("jim");
+
+let test = [];
+let bands: string[] = [];
+bands.push("van halen");
+
+//tuple
+let myTuple: [string, number, boolean] = ["Dave", 42, true];
+
+let mixed = ["John", 1, false];
+
+//OBJECTS
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: true,
+};
+
+exampleObj.prop2 = false;
