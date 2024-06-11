f5b1ee322517e5b29fb3d27a96e9f5d2a353224e
Clifford Musyoka <your_email@example.com>
2024-06-11 07:40:09 +0300

    Add type Aliases

---
+++ Functions/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'John';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('jim');
